63359e5663ab242d13070a2f7ecc96ca88582a26
ChrisRackauckas <accounts@chrisrackauckas.com>
2025-09-03 13:37:10 -0400

    Use Julia 1.10.10 LTS with error handling
    - Use exact LTS version 1.10.10 instead of generic 1.10
    - Add error handling for package installation failures
    - This should address R 4.5.0 compatibility while being more robust

--- R/diffeqr.R
+++ R/diffeqr.R
@@ -18,8 +18,14 @@
 #'
 #' @export
 diffeq_setup <- function (pkg_check=TRUE,...){
-  julia <- JuliaCall::julia_setup(installJulia=TRUE, version="1.10", ...)
-  if(pkg_check) JuliaCall::julia_install_package_if_needed("DifferentialEquations")
+  julia <- JuliaCall::julia_setup(installJulia=TRUE, version="1.10.10", ...)
+  if(pkg_check) {
+    tryCatch({
+      JuliaCall::julia_install_package_if_needed("DifferentialEquations")
+    }, error = function(e) {
+      warning("Package installation failed, trying to load existing installation: ", e$message)
+    })
+  }
   JuliaCall::julia_library("DifferentialEquations")
 
   functions <- JuliaCall::julia_eval("filter(isascii, replace.(string.(propertynames(DifferentialEquations)),\"!\"=>\"_bang\"))")
